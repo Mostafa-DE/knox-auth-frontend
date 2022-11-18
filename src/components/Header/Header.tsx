7ad10fa0b87c17ea6ddd16cd269a9d57d32c82d6
mostafa-de <mostafafayyado1@gmail.com>
2022-11-19 00:26:44 +0300

    change URL backend from heroku to railway app

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,52 +1,62 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import {Link} from "react-router-dom"
-import {useNavigate} from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { URL } from "../../constants";
 
-export default function Header({user}: any): JSX.Element {
-    const navigate = useNavigate()
-    const handleLogout = async () => {
-        const res = await fetch('https://djangodemoauth.herokuapp.com/api/auth/logout/', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        if (res.ok) navigate('/login');
-    }
+export default function Header({ user }: any): JSX.Element {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    const res = await fetch(`${URL}/api/auth/logout/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.ok) navigate("/login");
+  };
 
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        Auth-Demo
-                    </Typography>
-                    <a href="https://github.com/Mostafa-DE/simple-auth-django-react" target="_blank">
-                        <img
-                            src="/github.svg"
-                            alt="github img"
-                            width={30}
-                            height={30}
-                            style={{margin: "0 1rem 0 0"}}
-                        />
-                    </a>
-                    {!user && (
-                        <Box>
-                            <Button color="inherit"><Link to='/register'>Register</Link></Button>
-                            <Button color="inherit"><Link to='/login'>Login</Link></Button>
-                        </Box>
-                    )}
-                    {user && (
-                        <Button color="inherit" onClick={handleLogout}>Logout</Button>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Auth-Demo
+          </Typography>
+          <a
+            href="https://github.com/Mostafa-DE/simple-auth-django-react"
+            target="_blank"
+          >
+            <img
+              src="/github.svg"
+              alt="github img"
+              width={30}
+              height={30}
+              style={{ margin: "0 1rem 0 0" }}
+            />
+          </a>
+          {!user && (
+            <Box>
+              <Button color="inherit">
+                <Link to="/register">Register</Link>
+              </Button>
+              <Button color="inherit">
+                <Link to="/login">Login</Link>
+              </Button>
+            </Box>
+          )}
+          {user && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,134 +1,134 @@
 import {
-    Alert,
-    Avatar,
-    Box,
-    Button,
-    Container,
-    CssBaseline,
-    Snackbar,
-    TextField
-} from "@mui/material"
-import {useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Snackbar,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import BackdropLoading from "../BackdropLoading";
-
+import { URL } from "../../constants";
 
 const LoginForm = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [values, setValues] = useState({
-        username: '',
-        password: '',
-    })
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [openAlert, setOpenAlert] = useState(false);
-    const [openBackdrop, setOpenBackdrop] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
+  const [openBackdrop, setOpenBackdrop] = useState(false);
 
-    const handleChange = (event: any) => {
-        const {name, value} = event.target;
-        setValues({...values, [name]: value})
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    setOpenBackdrop(true);
+    const res = await fetch(`${URL}/api/auth/login/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        password: values.password,
+      }),
+    });
+    if (!res.ok) {
+      setOpenAlert(true);
+      setOpenBackdrop(false);
+      return;
     }
+    navigate("/");
+  };
 
-    const handleSubmit = async (event: any) => {
-        event.preventDefault();
-        setOpenBackdrop(true);
-        const res = await fetch('https://djangodemoauth.herokuapp.com/api/auth/login/', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: values.username,
-                password: values.password
-            })
-        })
-        if (!res.ok) {
-            setOpenAlert(true);
-            setOpenBackdrop(false);
-            return;
-        }
-        navigate('/')
-    }
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
 
-    const handleCloseAlert = () => {
-        setOpenAlert(false);
-    }
-
-    return (
-        <Container component="main" maxWidth="xs">
-            <BackdropLoading openBackdrop={openBackdrop}/>
-            <Snackbar anchorOrigin={{vertical: "top", horizontal: "center"}} open={openAlert} autoHideDuration={6000}
-                      onClose={handleCloseAlert}>
-                <Alert onClose={handleCloseAlert} severity="error" sx={{width: '100%'}}>
-                    Sorry, Invalid username or password Please try again.
-                </Alert>
-            </Snackbar>
-            <CssBaseline/>
-            <Box style={{
-                margin: "4rem 0 0 0",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column"
-            }}
-            >
-                <Avatar sx={{margin: "0 0 1rem 0"}}></Avatar>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        type='text'
-                        name="username"
-                        variant="outlined"
-                        fullWidth
-                        required
-                        onChange={handleChange}
-                        label="Username"
-                    />
-                    <TextField
-                        variant="outlined"
-                        required
-                        onChange={handleChange}
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        sx={{"margin": "1rem 0"}}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                    >
-                        Login
-                    </Button>
-                    <Button
-                        type="button"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{margin: "1rem 0 0 0"}}
-                    >
-                        <Link to='/register'>
-                            Go to register page
-                        </Link>
-                    </Button>
-                    <Button
-                        type="button"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{margin: "1rem 0 0 0"}}
-                    >
-                        <Link to='/'>
-                            Go to home page
-                        </Link>
-                    </Button>
-                </form>
-            </Box>
-        </Container>
-    )
-}
+  return (
+    <Container component="main" maxWidth="xs">
+      <BackdropLoading openBackdrop={openBackdrop} />
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={openAlert}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+      >
+        <Alert
+          onClose={handleCloseAlert}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          Sorry, Invalid username or password Please try again.
+        </Alert>
+      </Snackbar>
+      <CssBaseline />
+      <Box
+        style={{
+          margin: "4rem 0 0 0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Avatar sx={{ margin: "0 0 1rem 0" }}></Avatar>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            type="text"
+            name="username"
+            variant="outlined"
+            fullWidth
+            required
+            onChange={handleChange}
+            label="Username"
+          />
+          <TextField
+            variant="outlined"
+            required
+            onChange={handleChange}
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            sx={{ margin: "1rem 0" }}
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Login
+          </Button>
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ margin: "1rem 0 0 0" }}
+          >
+            <Link to="/register">Go to register page</Link>
+          </Button>
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ margin: "1rem 0 0 0" }}
+          >
+            <Link to="/">Go to home page</Link>
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+};
 
 export default LoginForm;
